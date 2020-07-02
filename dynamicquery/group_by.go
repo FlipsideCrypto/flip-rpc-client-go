bab9e45e80449e8901a0116809409ec8c05346c4
Jim Myers <jfmyers01@gmail.com>
2020-07-02 13:37:56 -0400

    Add start, end and gapfill to sdk.

--- dynamicquery/group_by.go
+++ dynamicquery/group_by.go
@@ -5,5 +5,8 @@ type GroupBy struct {
 	Field      string    `json:"field"`
 	Label      string    `json:"label"`
 	Timebucket string    `json:"timebucket"`
+	Gapfill    bool      `json:"gapfill"`
+	StartDate  string    `json:"start_date"`
+	EndDate    string    `json:"end_date"`
 	Datasets   []Dataset `json:"datasets"`
 }
